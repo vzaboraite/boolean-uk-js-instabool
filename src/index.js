3860033d030f758cd499f9e9223209d280dc4863
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-08-02 16:01:49 +0100

    Starting point pseudo-code.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-// write your code here
+/*
+1.0 to START THE SERVER run this command in vscode terminal:
+    json-server --watch db/db.json --routes db/routes.json
+
+2.0 use anchor element to append post articles:
+    const imageContainerElem = document.querrySelector(".image-container")
+
+3.0 Create renderImageCard():
+    - use example in index.html (<article class="image-card">...)
+
+    - input: -
+    - output: imageCardElem ?
+
+
+3.1 Create renderPosts():
+    - use renderImageCard()
+
+    - input: data from db.json ?
+    - output: -
+
+4.0 Create action functions:
+
+4.1 addLike():
+    - input: ?? 
+    - output: -
+
+4.2 addComment() ?
+
+5.0 The data must be persisted in server, after refresh it shouldn't go away!
+*/
