bfaf0261201641957a4c8769dfc3bc72f160a79b
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-08-02 18:26:07 +0100

    Get data from json; solve timing problem.

--- src/index.js
+++ src/index.js
@@ -30,3 +30,43 @@
 
 5.0 The data must be persisted in server, after refresh it shouldn't go away!
 */
+
+// DATA from db.json for images and comments
+
+let images = [];
+
+fetch("http://localhost:3000/images")
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log("Inside GET Fetch: ", data);
+
+    images = data;
+
+    // console.log("Timing problem solution: ", images);
+
+    // You only have access to the images array inside this .then function
+    // So where do you need to call your render functions???
+    renderPostCards(images);
+    renderCommentsSection(images);
+  });
+
+console.log("images: ", images);
+
+let comments = [];
+fetch("http://localhost:3000/comments")
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log("Inside GET Fetch: ", data);
+    // console.log("Comments inside fetch: ", data);
+    comments = data;
+
+    // renderCommentsSection(comments);
+  });
+
+console.log("comments: ", comments);
+
+// Anchor elements
+const imageContainerElem = document.querySelector(".image-container");
+
+// console.log(imageContainerElem);
+
